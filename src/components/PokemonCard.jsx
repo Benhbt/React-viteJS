b6a58f02490f988d3de7f1baaebb8bb87e8482a7
Benhbt <benji-hub@hotmail.fr>
2023-04-03 10:16:22 +0200

    Ajout d'une PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,29 @@
 function PokemonCard() {
+
+  const pokemon = pokemonList[0]; 
+
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-            alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            <img src={pokemon.imgSrc}
+            alt= "???" />
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
+
 }
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 export default PokemonCard; 
+
+
+
