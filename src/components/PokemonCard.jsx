ce9c0cda70796e4d387692f628727bf543331086
Benhbt <benji-hub@hotmail.fr>
2023-04-03 11:30:37 +0200

    passer des données d'un composant à un autre grâce aux props.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,17 @@
-function PokemonCard() {
+function PokemonCard(props) {
 
-  const pokemon = pokemonList[0]; 
+  console.log(props); 
+
 
     return (
         <figure>
-            <img src={pokemon.imgSrc}
+            <img src={props.pokemon.imgSrc}
             alt= "???" />
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{props.pokemon.name}</figcaption>
         </figure>
     )
 
 }
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
 export default PokemonCard;
 
